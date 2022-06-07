2512eade94d37c1f85046b34e61fb18c80b6e5ea
thomasvo <thomasttvo@gmail.com>
2022-06-07 11:54:41 -0700

    added server side

---
+++ example/server/src/index.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+import * as fs from 'fs';
+import path from 'path';
+
+const router = express.Router();
+
+router.get('/', (_, res) => {
+  res.send('Hello World!');
+});
+
+router.post('/upload', (req, res) => {
+  const filePath = path.join(__dirname, `/image.jpg`);
+  const stream = fs.createWriteStream('/dev/null');
+
+  if (req.query.simulateFailImmediately) {
+    res.status(500).send('Simulated Error').end();
+    return;
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    stream.on('open', () => {
+      console.log('Stream open ...  0.00%');
+      req.pipe(stream);
+    });
+
+    // Drain is fired whenever a data chunk is written.
+    stream.on('drain', () => {
+      // TODO simulate error while uploading
+      const written = stream.bytesWritten;
+      const total = parseInt(req.headers['content-length']!, 10);
+      const progress = (written / total) * 100;
+
+      console.log(`Processing  ...  ${progress.toFixed(2)}%`);
+      if (req.query.simulateFailMidway && progress > 50) {
+        res.status(500).send('Simulated Error').end();
+        resolve();
+      }
+    });
+
+    stream.on('close', () => {
+      console.log('Processing  ...  100%');
+      res.send(filePath);
+      resolve();
+    });
+
+    stream.on('error', (err) => {
+      console.error(err);
+      reject(err);
+    });
+  });
+});
+
+const app = express();
+app.use(router);
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
---
+++ lib/src/index.d.ts
@@ -0,0 +1,18 @@
+import { AddListener, ChunkInfo, FileInfo, MultipartUploadOptions, UploadId, UploadOptions } from '../types';
+export declare const getFileInfo: (path: string) => Promise<FileInfo>;
+export declare const startUpload: ({ path, ...options }: UploadOptions | MultipartUploadOptions) => Promise<UploadId>;
+export declare const cancelUpload: (cancelUploadId: string) => Promise<boolean>;
+export declare const addListener: AddListener;
+export declare const ios: {
+    chunkFile: (parentFilePath: string, chunkDirPath: string, numChunks: number) => Promise<ChunkInfo[]>;
+};
+declare const _default: {
+    startUpload: ({ path, ...options }: UploadOptions | MultipartUploadOptions) => Promise<string>;
+    cancelUpload: (cancelUploadId: string) => Promise<boolean>;
+    addListener: AddListener;
+    getFileInfo: (path: string) => Promise<FileInfo>;
+    ios: {
+        chunkFile: (parentFilePath: string, chunkDirPath: string, numChunks: number) => Promise<ChunkInfo[]>;
+    };
+};
+export default _default;
